24ee103231c630054fc9a4a8b50c00afd1583b8f
Snow <6154363+abeswz@users.noreply.github.com>
2026-05-02 15:42:25 -0300

    chore(threshold): add in advance the th of score

--- app/service/score.py
+++ app/service/score.py
@@ -1,8 +1,13 @@
 from app.schemas.fraud_score import FraudScore
 from app.schemas.transaction import Transaction
 
+# Fix value
+# Reference: https://github.com/zanfranceschi/rinha-de-backend-2026/blob/main/docs/br/REGRAS_DE_DETECCAO.md
+THRESHOLD = 0.6
+
 
 def process_score(
     transaction: Transaction, similar_transactions: list[Transaction]
 ) -> FraudScore:
-    return FraudScore(approved=True, fraud_score=100.0)
+    score = 1.0
+    return FraudScore(approved=score < THRESHOLD, fraud_score=score)
